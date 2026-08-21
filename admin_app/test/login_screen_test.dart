d6de50d2581fda5e411e6b95d1b00e2680d193ea
freetux <thefreetux@gmail.com>
2026-08-21 11:41:58 +0330

    test: add Flutter authentication screen coverage

---
+++ admin_app/test/login_screen_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:soransib/screens/login_screen.dart';
+
+void main() {
+  testWidgets('login screen renders registration controls', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoginScreen()));
+    expect(find.text('ورود به SoranSib'), findsOneWidget);
+    expect(find.text('حساب کاربری ندارم'), findsOneWidget);
+  });
+}
