be0e39048c1f5899845cf63753adb8ba0b6e1cdf
freetux <thefreetux@gmail.com>
2026-08-21 19:35:24 +0330

    test: avoid generated MaterialApp test dependency

--- admin_app/test/login_screen_test.dart
+++ admin_app/test/login_screen_test.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:soransib/screens/login_screen.dart';
 
 void main() {
   testWidgets('login screen renders registration controls', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: LoginScreen()));
+    await tester.pumpWidget(const LoginScreen());
     expect(find.text('ورود به SoranSib'), findsOneWidget);
     expect(find.text('حساب کاربری ندارم'), findsOneWidget);
   });
